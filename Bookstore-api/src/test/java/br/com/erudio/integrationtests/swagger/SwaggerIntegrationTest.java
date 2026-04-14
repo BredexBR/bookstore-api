8b12b647b1d3626a19a8968b45bd818efd03aaa0
BredexBR <breno.ferreira.bre2@gmail.com>
2026-04-14 15:11:58 -0300

    Fix: Adicionando variáveis de ambiente para github Actions, corrigindo porta de entrada no teste do swagger

--- Bookstore-api/src/test/java/br/com/erudio/integrationtests/swagger/SwaggerIntegrationTest.java
+++ Bookstore-api/src/test/java/br/com/erudio/integrationtests/swagger/SwaggerIntegrationTest.java
@@ -1,21 +1,24 @@
 package br.com.erudio.integrationtests.swagger;
 
-import br.com.erudio.config.TestConfigs;
 import br.com.erudio.integrationtests.testcontainers.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SwaggerIntegrationTest extends AbstractIntegrationTest {
+
+	@LocalServerPort
+	private int randomPort;
 
 	@Test
 	void shouldDisplaySwaggerUIPage() {
 		var content = given()
 				.basePath("/swagger-ui/index.html")
-				.port(TestConfigs.SERVER_PORT)
+				.port(randomPort)
 				.when()
 				.get()
 				.then()
